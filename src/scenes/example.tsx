2042fbaa6350fdfc27aa390c0914596822e0d142
MiguelCock <macockc@eafit.edu.co>
2024-11-03 15:57:03 -0500

    triying sume codo in the video

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,12 +1,39 @@
-import {Circle, makeScene2D} from '@motion-canvas/2d';
-import {createRef} from '@motion-canvas/core';
+import {makeScene2D, Code, replace, Circle} from '@motion-canvas/2d';
+import {all, createRef, DEFAULT, waitFor} from '@motion-canvas/core';
 
 export default makeScene2D(function* (view) {
-  // Create your animations here
+  let num = 1;
 
-  const circle = createRef<Circle>();
+  const code = createRef<Code>();
+  view.add(
+    <Code
+      ref={code}
+      fontSize={28}
+      fontFamily={'JetBrains Mono, monospace'}
+      offsetX={-1}
+      x={-600}
+      code={`let radius = ${num};`}
+    />,
+  );
 
-  view.add(<Circle ref={circle} size={320} fill={'lightseagreen'} />);
+  const myCircle = createRef<Circle>();
+  view.add(
+    <Circle
+      ref={myCircle}
+      // try changing these properties:
+      x={400}
+      width={num}
+      height={num}
+      fill="#e13238"
+    />,
+  );
 
-  yield* circle().scale(2, 2).to(1, 2);
+  for(let i = 0; i < 999; i++) {
+    yield* all(
+      code().code.edit(0.01)`let radius = ${replace(String(num), String(num+=1))};`,
+      myCircle().scale(num, 0.01)
+    )
+  }
+
+  yield* waitFor(0.6);
 });
